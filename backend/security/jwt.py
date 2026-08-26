c200e17174987d7689e44e676d92d2712da2175d
komal692007 <ky4403185@gmail.com>
2026-08-26 07:02:11 +0530

    Add files via upload

---
+++ backend/security/jwt.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta, timezone
+from jose import jwt
+
+
+SECRET_KEY = "change-this-to-a-random-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
